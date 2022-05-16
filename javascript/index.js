87d0fc07f71087221efe15c9e1b1ce85c6b387fe
shindo-develop <shindo.develop@gmail.com>
2022-05-16 20:00:07 +0900

    add google

--- javascript/index.js
+++ javascript/index.js
@@ -16,3 +16,14 @@ content.appleID.addEventListener('click', (event) => {
     globalFunc.displayBlock(htmlField.contentTable);
     createContentTable(appleIDJson);
 });
+
+content.google.addEventListener('click', (event) => {
+    // htmlのidはvalue.path[0].idに相当する
+    // value.path[0].idは2023年にアクセス不可のapiになるため
+    // event.composedPath()[0].idを使用
+    // console.log(value.path[0].id);
+    let googleJson = excelJson[event.composedPath()[0].id];
+    globalFunc.displayNone(htmlField.contentGroup);
+    globalFunc.displayBlock(htmlField.contentTable);
+    createContentTable(googleJson);
+});
