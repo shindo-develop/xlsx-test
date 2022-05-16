3c7e1aab75fc8faf2ddee00fbf7ea87b33b1a358
shindo-develop <shindo.develop@gmail.com>
2022-05-17 00:24:38 +0900

    検索ジャンプ機能追加 fixes #19

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 import { excelJson } from './excel.js';
-import { htmlField, globalFunc } from './config.js';
+import { htmlField, globalFunc, isExport } from './config.js';
 import { createContentTable} from './template.js';
 
 swal('【重要】FAQエクセルファイルを読み込んでから使用してください。\n①赤い帯のナビゲーションタグのダウンロードボタンからエクセルファイルを読み込む\n②ダウンロードボタンの横にあるインプットボタンを押す')
@@ -9,9 +9,20 @@ globalFunc.returnPage();
 // card
 for(let i = 0; i < document.querySelectorAll('.contents-move').length; i++){
     document.querySelectorAll('.contents-move')[i].addEventListener('click', (event) => {
-        const contentJson = excelJson[event.composedPath()[0].id];
+        const contentName = event.composedPath()[0].id
+        const contentJson = excelJson[contentName];
         globalFunc.displayNone(htmlField.contentGroup);
         globalFunc.displayBlock(htmlField.contentTable);
-        createContentTable(contentJson);
+        createContentTable(contentName, contentJson);
     });
 };
+
+// option
+document.getElementById('floatingSearch').addEventListener('change', (event) => {
+    const contentName = event.composedPath()[0].value
+    const contentJson = excelJson[contentName];
+    globalFunc.displayNone(htmlField.contentGroup);
+    globalFunc.displayBlock(htmlField.contentTable);
+    if(isExport.content)globalFunc.displayNone(htmlField.content);
+    createContentTable(contentName, contentJson);
+});
